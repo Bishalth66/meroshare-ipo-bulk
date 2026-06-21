33d0bed088119f89c8445b66ad1fdfd8a6084fda
Bishal Thapa <bishalth66@gmail.com>
2026-06-21 20:07:11 +0545

    feat: playwright code to open meroshare

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+
+BASE_URL = "https://meroshare.cdsc.com.np/#/login" 
+
 def main():
-    print("Hello from backend!")
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+        page = browser.new_page()
+        page.goto(BASE_URL)
+        print(page.title())
+        browser.close()         
 
 
 if __name__ == "__main__":
