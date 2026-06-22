cc775c91e2e393cfa48d38bf6058caf44c09a0ab
Bishal Thapa <bishalth66@gmail.com>
2026-06-22 10:14:36 +0545

    Added Login

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,26 @@
-from playwright.sync_api import sync_playwright
+import os
+from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright, TimeoutError 
 
-
-BASE_URL = "https://meroshare.cdsc.com.np/#/login" 
+load_dotenv()
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
-        page.goto(BASE_URL)
-        print(page.title())
-        browser.close()         
-
+        page.goto(os.getenv("BASE_URL"))
+        try:
+           page.wait_for_selector(".select2-selection", timeout=5000)
+           page.click(".select2-selection")
+           page.wait_for_selector(".select2-search__field", timeout=5000)
+           page.fill(".select2-search__field", str(os.getenv("DP")))
+           page.keyboard.press("Enter")
+           page.fill("#username", os.getenv("USER"))
+           page.fill("#password", os.getenv("PASSWORD"))
+           page.click('button[type="submit"]')
+        except TimeoutError:
+            pass        
+    
 
 if __name__ == "__main__":
     main()
